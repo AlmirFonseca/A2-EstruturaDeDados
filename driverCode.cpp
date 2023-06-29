47f5afe813523043ce9448926e1fd55497948e28
gustavorrocha <gustavoreisrocha.grr@gmail.com>
2023-06-29 18:50:39 -0300

    Evidencie as separações entre os testes das funções

--- driverCode.cpp
+++ driverCode.cpp
@@ -3,45 +3,61 @@
 #include <iostream>
 
 int main()
-{
-    
-    // Cria uma árvore binária de busca a partir de dados digitados pelo usuário
-    struct Node* root = arvoreDados();
-    cout<<"Árvore binária de busca criada: ";
-    printTree(root);
-    // Verifica se a árvore binária de busca é perfeita
-    cout<<"A árvore binária de busca é perfeita? "<<ePerfeita(root)<<endl;
+{   
+    // =============== CRIAÇÃO DA ÁRVORE POR INPUT ===============
+    cout << endl << "---- Criação de árvore por input: " << endl;
 
-    cout<<endl;
+    // Cria uma árvore binária de busca a partir de dados digitados pelo usuário
+    struct Node* ptrRootInput = inputTree();
+    cout << "Árvore binária de busca criada: ";
+    printTree(ptrRootInput);
+    // Verifica se a árvore binária de busca é perfeita
+    cout << endl << "A árvore binária de busca é perfeita? " << ePerfeita(ptrRootInput) << endl;
+
+
+    // =============== CRIAÇÃO DA ÁRVORE POR TXT ===============
+    cout << endl << "---- Criação de árvore por txt: " << endl;
 
     // Cria uma árvore binária de busca a partir de um arquivo .txt
-    struct Node* ptrNode2 = ArvoreTxt("./arvore.txt");
-    cout<<"Árvore binária de busca criada a partir de um arquivo .txt:"<<endl;
-    printTree(ptrNode2);
+    struct Node* ptrRootTxt = txtToTree("./arvore.txt");
+    cout << "Árvore binária de busca criada a partir de um arquivo .txt:";
+    printTree(ptrRootTxt);
     // Verifica se a árvore binária de busca é perfeita
-    cout<<"A árvore binária de busca é perfeita? "<<ePerfeita(ptrNode2)<<endl;
+    cout << endl << "A árvore binária de busca é perfeita? " << ePerfeita(ptrRootTxt) << endl;
+
 
     // =============== INSERÇÃO DE ELEMENTO ===============
     cout << endl << "---- Inserção de elemento: " << endl;
 
+    // Imprime a árvore antes da inserção
+    cout << "Árvore binária de busca antes da inserção do elemento: ";
+    printTree(ptrRootTxt);
+    cout << endl;
+
     // Solicita ao usuário um elemento para ser inserido na árvore
-    ptrNode2 = inputInsertNode(ptrNode2);
+    ptrRootTxt = inputInsertNode(ptrRootTxt);
 
     // Imprime a árvore com o novo elemento inserido
     cout << "Árvore binária de busca após a inserção do elemento: ";
-    printTree(ptrNode2);
+    printTree(ptrRootTxt);
 
     cout << endl;
+
 
     // =============== REMOÇÃO DE ELEMENTO ===============
     cout << endl << "---- Remoção de elemento: " << endl;
 
+    // Imprime a árvore antes da remoção
+    cout << "Árvore binária de busca antes da remoção do elemento: ";
+    printTree(ptrRootTxt);
+    cout << endl;
+
     // Solicita ao usuário um elemento para ser removido da árvore
-    ptrNode2 = inputRemoveNode(ptrNode2);
+    ptrRootTxt = inputRemoveNode(ptrRootTxt);
 
     // Imprime a árvore com o elemento removido
     cout << "Árvore binária de busca após a remoção do elemento: ";
-    printTree(ptrNode2);
+    printTree(ptrRootTxt);
 
     cout << endl << endl;
 
@@ -59,7 +75,7 @@ int main()
     printList(ptrList);
 
     // Converte a árvore binária de busca em uma lista encadeada simples
-    struct List* ptrList2 = converterArvore(ptrNode2);
+    struct List* ptrList2 = converterArvore(ptrRootTxt);
     // Imprime a lista encadeada simples
     cout<<"Lista encadeada simples criada a partir da árvore binária de busca: ";
     printList(ptrList2);
@@ -68,7 +84,7 @@ int main()
     cout<<"Lista encadeada simples antes do ordenamento por Insertion Sort: ";
     printList(ptrList2);
 
-    struct List* ptrList3 = insertionSort(ptrNode2);
+    struct List* ptrList3 = insertionSort(ptrRootTxt);
 
     // Imprime a lista encadeada simples
     cout<<"Lista encadeada simples ordenada utilizando Insertion Sort: ";
