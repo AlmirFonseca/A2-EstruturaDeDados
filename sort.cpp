0eb2347f58abd78a80c149e71a883f27f8f864c7
AlmirFonseca <almirfonseca60@gmail.com>
2023-06-29 21:29:58 -0300

    Atualize a base de listas para listas duplamente encadeadas

--- sort.cpp
+++ sort.cpp
@@ -3,162 +3,111 @@
 
 using namespace std;
 
-// Função para criar um novo nó
-struct ListNode* newListNode(int iPayload)
+// Cria um novo nó e carrega o payload
+struct ListNode* createListNode(int iPayload)
 {
-    // Aloca memória para o novo nó
-    struct ListNode* newNode = new struct ListNode; 
-
-    // Atribui o dado ao novo nó
-    newNode->iData = iPayload;
-
-    // Atribui nullptr ao ponteiro para o próximo nó
-    newNode->ptrNext = nullptr;
-    return newNode;
-}
-
-// Função para criar uma nova lista
-struct List* newList()
-{
-    // Aloca memória para a nova lista
-    struct List* newList = new List;
-
-    // Atribui nullptr ao ponteiro para o primeiro nó
-    newList->ptrHead = nullptr;
-    return newList;
-}
-
-// Função para inserir um novo nó no final da lista
-void insertListNode(struct List* ptrList, int iPayload)
-{
-    // Cria um novo nó
-    struct ListNode* newNode = newListNode(iPayload);
-
-    // Cria um ponteiro para percorrer a lista
-    struct ListNode* ptrCurrent = ptrList->ptrHead;
-
-    // Se a lista estiver vazia, o novo nó é o primeiro nó da lista
-    if (ptrCurrent == nullptr)
+    // Aloca a memória necessária para o novo nó
+    struct ListNode* ptrNewNode = (struct ListNode*) malloc(sizeof(struct ListNode));
+    
+    // Caso a alocação falhe
+    if (ptrNewNode == nullptr)
     {
-        ptrList->ptrHead = newNode;
+        cout << "Ocorreu um erro na alocação de memória para um novo nó da lista" << endl;
+        return nullptr;
     }
-    // Caso contrário, percorre a lista até o último nó
+
+    // Caso a alocação tenha sido bem sucedida, carrega o payload e inicializa os ponteiros com valor nulo
+    ptrNewNode->iData = iPayload;
+    ptrNewNode->ptrNext = nullptr;
+    ptrNewNode->ptrPrev = nullptr;
+    
+    // Retorna o ponteiro para o novo nó
+    return ptrNewNode;
+}
+
+// Insere um nó no final da lista
+void insertList(struct ListNode** ptrHead, int iPayload)
+{
+    // Cria um novo nó e carrega o seu payload
+    struct ListNode* ptrNewNode = createListNode(iPayload);
+    
+    // Como ele vai ser o último elemento, seu ptrNext = nullptr
+    ptrNewNode->ptrNext = nullptr;
+    
+    // Caso não existisse nenhum nó na lista
+    if ((*ptrHead) == nullptr)
+    {
+        // O ptrPrev do novo nó aponta para nulo
+        ptrNewNode->ptrPrev = nullptr;
+        // E o ptrHead aponta pra ele
+        (*ptrHead) = ptrNewNode; 
+    }
+    // Caso já existisse algum nó na lista
     else
     {
-        while (ptrCurrent->ptrNext != nullptr)
+        //Precisamos de um ponteiro auxiliar para caminhar até o fim da lista
+        struct ListNode* ptrTemp = (*ptrHead);
+        
+        // Atualizamos seu valor até atingir o último nó
+        while(ptrTemp->ptrNext != nullptr) 
         {
-            ptrCurrent = ptrCurrent->ptrNext;
+            ptrTemp = ptrTemp->ptrNext;
         }
-        // Atribui o novo nó ao ponteiro para o próximo nó do último nó da lista
-        ptrCurrent->ptrNext = newNode;
+
+        // Atualiza o ponteiro do antigo último elemento 
+        ptrTemp->ptrNext = ptrNewNode;
+        // Atualiza o ponteiro do atual último elemento
+        ptrNewNode->ptrPrev = ptrTemp;
     }
 }
 
-// Função para imprimir a lista
-void printList(struct List* ptrList)
+// Deleta toda a lista
+void deleteLinkedList(struct ListNode** ptrHead)
 {
-    // Cria um ponteiro para percorrer a lista
-    struct ListNode* ptrCurrent = ptrList->ptrHead;
+    // Criamos 2 ponteiros auxiliares para apontar o nó atual (que será removido) e o nó seguinte
+    struct ListNode* ptrCurrentNode = *ptrHead;
+    struct ListNode* ptrNextNode;
+
+    // Itera sobre toda a lista
+    while (ptrCurrentNode != nullptr)
+    {
+        // Posiciona o ponteiro ptrCurrentNode à frente do ponteiro ptrCurrentNode
+        ptrNextNode = ptrCurrentNode->ptrNext;
+
+        // Libera a memória do ponteiro ptrCurrentNode
+        free(ptrCurrentNode);
+
+        // Atualiza o valor de ptrCurrentNode, avançando para o próximo nó
+        ptrCurrentNode = ptrNextNode;
+    }
+
+    // Por fim, atualiza o valor de ptrHead para nullptr
+    *ptrHead = nullptr;
+}
+
+// Exibe os elementos da lista
+void printList(struct ListNode* ptrHead)
+{
 
     // Se a lista estiver vazia, imprime uma mensagem
-    if (ptrList->ptrHead == nullptr)
+    if (ptrHead == nullptr)
     {
         cout << "Lista vazia" << endl;
         return;
     }
-    // Caso contrário, percorre a lista e imprime os dados
-    while (ptrCurrent != nullptr)
+
+    // Se o ponteiro inserido não for o primeiro da lista, imprime uma mensagem
+    if (ptrHead->ptrPrev != nullptr)
     {
-        cout << ptrCurrent->iData << " ";
-        ptrCurrent = ptrCurrent->ptrNext;
+        cout << "Meio da lista" << endl;
+        return;
+    }
+
+    // Caso contrário, percorre a lista e imprime os dados
+    while (ptrHead != nullptr)
+    {
+        cout << ptrHead->iData << " ";
+        ptrHead = ptrHead->ptrNext;
     }
     cout << endl;
-}
-
-// Função que percorre a árvore binária de busca e insere os dados em uma lista
-void insertTreeInList(struct Node* ptrNodeTree, struct List* ptrList)
-{
-    // Se o nó for diferente de nullptr, percorre a árvore
-    if (ptrNodeTree != nullptr)
-    {
-        // Percorre a árvore em pós-ordem
-        insertTreeInList(ptrNodeTree->ptrLeft, ptrList);
-        insertTreeInList(ptrNodeTree->ptrRight, ptrList);
-
-        // Insere o dado do nó na lista
-        insertListNode(ptrList, ptrNodeTree->iData);
-    }
-}
-
-// Função que converte uma árvore binária de busca em uma lista encadeada
-struct List* treeToList(struct Node* ptrNodeTree)
-{
-    // Cria uma nova lista
-    struct List* ptrList = newList();
-
-    // Se o nó for nullptr, retorna a lista vazia
-    if (ptrNodeTree == nullptr)
-    {
-        return ptrList;
-    }
-
-    // Caso contrário, percorre a árvore e insere os dados na lista
-    insertTreeInList(ptrNodeTree, ptrList);
-
-    return ptrList;
-}
-
-// Função que ordena uma lista utilizando Insertion Sort
-struct List* insertionSort(struct Node* ptrNodeTree)
-{
-    // Converte a árvore binária de busca em uma lista encadeada
-    struct List* ptrList = treeToList(ptrNodeTree);
-
-    // Cria um ponteiro para percorrer a lista não ordenada
-    struct ListNode* ptrCurrent = ptrList->ptrHead;
-
-    // Caso a lista esteja vazia ou tenha só um elemento, retorna ela mesma
-    if (ptrCurrent == nullptr || ptrCurrent->ptrNext == nullptr)
-    {
-        return ptrList;
-    }
-
-    // Caso contrário, cria uma nova lista vazia para armazenar a lista ordenada
-    struct List* ptrOrderedList = newList();
-    
-    // Percorre a lista não ordenada
-    while (ptrCurrent != nullptr) 
-    {
-        // Cria um ponteiro para o próximo nó da lista não ordenada
-        struct ListNode* ptrNextNode = ptrCurrent -> ptrNext;
-
-        // Caso a lista ordenada esteja vazia ou o dado do nó atual seja menor que o dado do primeiro nó da lista ordenada
-        if (ptrOrderedList->ptrHead == nullptr || ptrCurrent->iData < ptrOrderedList->ptrHead->iData)
-        {
-            // Insere o nó no início da lista ordenada
-            ptrCurrent->ptrNext = ptrOrderedList->ptrHead;
-            ptrOrderedList->ptrHead = ptrCurrent;
-        }
-        else
-        {
-            // Cria um ponteiro para percorrer a lista ordenada
-            struct ListNode* ptrTemp = ptrOrderedList->ptrHead;
-
-            // Percorre a lista ordenada até encontrar a posição correta para inserir o nó
-            while (ptrTemp->ptrNext != nullptr && ptrCurrent->iData >= ptrTemp->ptrNext->iData)
-            {
-                ptrTemp = ptrTemp->ptrNext;
-            }
-            // Insere o nó na posição correta da lista ordenada
-            ptrCurrent->ptrNext = ptrTemp->ptrNext;
-            ptrTemp->ptrNext = ptrCurrent;
-        }
-        // Atualiza o ponteiro para o próximo nó da lista não ordenada
-        ptrCurrent = ptrNextNode;
-    }
-
-    // Atualiza o ponteiro head da lista para a lista ordenada
-    ptrList->ptrHead = ptrOrderedList->ptrHead;
-
-    return ptrList;
 }
--- sort.h
+++ sort.h
@@ -5,32 +5,18 @@ struct ListNode
     int iData;
     // Ponteiro para o próximo nó
     struct ListNode* ptrNext;
+    // Ponteiro para o nó anterior
+    struct ListNode* ptrPrev;
 };
 
-// Estrutura para uma lista encadeada (armazena o ponteiro para o primeiro nó da lista)
-struct List
-{
-    // Ponteiro para o primeiro nó da lista
-    struct ListNode* ptrHead;
-};
+// Cria um novo nó e carrega o payload
+struct ListNode* createListNode(int iPayload)
 
-// Função para criar um novo nó
-struct ListNode* newListNode(int iPayload);
+// Insere um nó no final da lista
+void insertList(struct ListNode** ptrHead, int iPayload)
 
-// Função para criar uma nova lista
-struct List* newList();
+// Deleta toda a lista
+void deleteLinkedList(struct ListNode** ptrHead)
 
-// Função para inserir um novo nó no final da lista
-void insertListNode(struct List* ptrList, int iPayload);
-
-// Função para imprimir a lista
-void printList(struct List* ptrList);
-
-// Função que percorre a árvore binária de busca e insere os dados em uma lista
-void insertTreeInList(struct Node* ptrNodeTree, struct List* ptrList);
-
-// Função que converte uma árvore binária de busca em uma lista encadeada
-struct List* treeToList(struct Node* ptrNodeTree);
-
-// Função que transforma uma árvore binária em lista encadeada e a ordena utilizando Insertion Sort
-struct List* insertionSort(struct Node* ptrNodeTree);
+// Exibe os elementos da lista
+void printList(struct ListNode* ptrHead)
