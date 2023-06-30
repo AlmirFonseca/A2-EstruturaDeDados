96cd6340ab2930e90e2d9a9edaf169b1fef5c37b
gustavorrocha <gustavoreisrocha.grr@gmail.com>
2023-06-30 16:20:33 -0300

    Altere a codificação antes de exibir o menu

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
+#include <windows.h>
 #include "src/menu.h" // Inclui as funções do menu
 
 using namespace std;
 
 int main()
 {
+    // Define a codificação do terminal para UTF-8
+    system("chcp 65001");
+
+    // Limpa o terminal
+    system("cls");
+
     // Chama a função que exibe o menu principal
     menuPrincipal();
 
